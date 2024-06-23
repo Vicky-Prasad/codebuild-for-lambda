679146ded9b252a6bf1248afbe313e1a57d27b16
Vicky Prasad <vickyprasad454@gmail.com>
2024-06-23 02:05:34 -0600

    updating message version

--- lambda_function.py
+++ lambda_function.py
@@ -4,6 +4,6 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda v2!')
+        'body': json.dumps('Hello from Lambda v3!')
     }
 
